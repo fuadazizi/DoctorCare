98c5c47800a8453b086b92f07151e22f189537f2
Indra <indrawahyudi2710@gmail.com>
2020-05-02 19:54:10 +0700

    Update Slider

--- application/views/template/slider.php
+++ application/views/template/slider.php
@@ -13,7 +13,7 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" id="item">
-      <img class="d-block w-100" src="<?= base_url(); ?>/assets/pic/slider/slider-bg-6.jpg" alt="First slide">
+      <img class="d-block w-100" src="<?= base_url(); ?>/assets/pic/slider/slider-bg-5.png" alt="First slide">
       <div class="carousel-caption d-none d-md-block">
         <h3> Selamat datang, <?php echo $this->session->userdata('session_nama');?></h3>
         <p > Anda masuk sebagai <?= $person ?>
@@ -21,14 +21,14 @@
       </div>
     </div>
     <div class="carousel-item" id="item">
-      <img class="d-block w-100" src="<?= base_url(); ?>/assets/pic/slider/slider-bg-5.png" alt="Second slide">
+      <img class="d-block w-100" src="<?= base_url(); ?>/assets/pic/slider/slider-bg-6.jpg" alt="Second slide">
       <div class="carousel-caption d-none d-md-block">
         <h5> Dokter </h5>
         <p> Dokter adalah seseorang yang sangat sakti. Dengan ilmu dia bisa membantu menyelamatkan orang, tentunya dengan seizin Allah SWT. Tapi sebagai dokter tidaklah mudah, karena mereka berurusan dengan nyawa. Salah sedikit saja, mereka bisa membahayakan nyawa orang lain.</p>
       </div>
     </div>
     <div class="carousel-item" id="item">
-      <img class="d-block w-100" src="<?= base_url(); ?>/assets/pic/slider/slider-bg-7.jpg" alt="Third slide">
+      <img class="d-block w-100" src="<?= base_url(); ?>/assets/pic/slider/slider-bg-8.png" alt="Third slide">
         <div class="carousel-caption d-none d-md-block">
         <h5> Pasien </h5>
         <p>Pasien adalah seseorang yang menerima perawatan medis. Sering kali, pasien menderita penyakit atau cedera dan memerlukan bantuan dokter untuk memulihkannya.</p>
